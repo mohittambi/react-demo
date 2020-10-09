7671ff0837e7d0f650c6889e10a2027a794fffa5
mohit tambi <er.mohittambi@gmail.com>
2020-10-09 20:28:08 +0530

    merge branch wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
 import { useHistory } from "react-router-dom";
-import Login from "./login";
 import Footer from "./components/Footer";
-import Home from "./home";
+import Main from "./main";
 
 const App = () => {
   const history = useHistory();
@@ -16,7 +13,7 @@ const App = () => {
 
   return (
     <>
-      <Home />
+      <Main />
       <Footer />
     </>
   );
